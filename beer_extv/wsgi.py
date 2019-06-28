d4cef8134390437ea6052ad9f73ad706be5d3415
Sladyn <gunnerforlife00@gmail.com>
2019-06-29 00:56:16 +0530

    Added Front-end Application and set up Django Backend

--- beer_extv/wsgi.py
+++ beer_extv/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for beer_extravaganza project.
+WSGI config for beer_extv project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beer_extravaganza.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beer_extv.settings')
 
 application = get_wsgi_application()
